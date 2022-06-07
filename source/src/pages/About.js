dbcbcfd7e24f900273414b96d7f60729e7c23d81
BabdCatha <36963178+BabdCatha@users.noreply.github.com>
2022-06-07 09:39:10 +0200

    Fixed the about page
    Added two packages to do this : rehype-raw and rehype-sanitize.

--- source/src/pages/About.js
+++ source/src/pages/About.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ReactMarkdown from 'react-markdown';
+import Markdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import rehypeSanitize from 'rehype-sanitize';
 import raw from 'raw.macro';
 
 import Main from '../layouts/Main';
@@ -11,9 +13,6 @@ const markdown = raw('../data/about.md');
 const count = markdown.split(/\s+/)
   .map((s) => s.replace(/\W/g, ''))
   .filter((s) => s.length).length;
-
-// Make all hrefs react router links
-const LinkRenderer = ({ ...children }) => <Link {...children} />;
 
 const About = () => (
   <Main
@@ -27,13 +26,7 @@ const About = () => (
           <p>(in about {count} words)</p>
         </div>
       </header>
-      <ReactMarkdown
-        source={markdown}
-        renderers={{
-          Link: LinkRenderer,
-        }}
-        escapeHtml={false}
-      />
+      <Markdown rehypePlugins={[rehypeRaw, rehypeSanitize]}>{markdown}</Markdown>
     </article>
   </Main>
 );
